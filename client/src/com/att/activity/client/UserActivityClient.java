a13c91d90754d551ba7f251d4c19e276d191c862
Siva Kumar K <sivakumar.kumaravelu@gmail.com>
2018-06-14 10:37:05 -0400

    Update UserActivityClient.java

--- client/src/com/att/activity/client/UserActivityClient.java
+++ client/src/com/att/activity/client/UserActivityClient.java
@@ -5,9 +5,14 @@ import java.net.URL;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * 
+ * @author Sivakumar Kumaravelu
+ *
+ */
 
 public class UserActivityClient {
 
@@ -19,32 +24,31 @@ public class UserActivityClient {
 			URL url = new URL(uri);
 			ObjectMapper objectMapper = new ObjectMapper();				
 			List<Map<String, Object>> jsonList = objectMapper.readValue(url, new TypeReference<List<Map<String, Object>>>(){});
-
 			Map<String, Object> element = null;
 			String key;
-			int count=0;
 			int total=0;
-
 			for (int i = 0; i < jsonList.size(); i++) {
 				element = jsonList.get(i);
 				for (Map.Entry<String, Object> entry : element.entrySet())
 				{	
 					key = entry.getKey();
 					System.out.print("Key: "+key);
-					System.out.println("  ---- Value :"+entry.getValue());
+					System.out.println("  ----> Value :"+entry.getValue());
 					//numbers kept part of JSON, if not process separately
 					if(key.equalsIgnoreCase("numbers")) {
 						@SuppressWarnings("unchecked")
 						List<Integer> numList = (List<Integer>)entry.getValue();
+						int count=0;
+						int sum=0;
 						for (int j = 0; j < numList.size(); j++) {
-							count += j;
-							total +=numList.get(j);
+							sum = numList.get(j);
+							count += sum;
+							total += sum;							
 						}
+						System.out.println("Sum: "+count+"\n-----------------------------");
 					}
 				}
 			}
-			System.out.print("------------------------\n");
-			System.out.print("Count : "+count+"\n");
 			System.out.println("Total : "+total);
 		} catch (IOException e) {
 			e.printStackTrace();
